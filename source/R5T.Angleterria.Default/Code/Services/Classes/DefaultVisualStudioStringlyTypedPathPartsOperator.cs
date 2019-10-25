445c1ac82029fccaa784d20f5674608e1b4c4371
MinexAutomation <minex.automation@gmail.com>
2019-10-24 18:11:36 -0700

    Added default implementation of a Visual Studio stringly-typed path parts operator.

---
+++ source/R5T.Angleterria.Default/Code/Services/Classes/DefaultVisualStudioStringlyTypedPathPartsOperator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace R5T.Angleterria.Default
+{
+    public class DefaultVisualStudioStringlyTypedPathPartsOperator : IVisualStudioStringlyTypedPathPartsOperator
+    {
+        /// <summary>
+        /// The default project directory name is the same as the project name.
+        /// </summary>
+        public string GetProjectDirectoryNameForProjectName(string projectName)
+        {
+            var projectDirectoryName = projectName;
+            return projectName;
+        }
+    }
+}
