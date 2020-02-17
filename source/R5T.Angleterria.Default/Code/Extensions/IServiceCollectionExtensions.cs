31c1c359fbcbf69a052bd6c606b1c8f2c4d3ec7a
MinexAutomation <minex.automation@gmail.com>
2020-02-17 15:36:56 -0800

    Added IServiceCollection extensions.

---
+++ source/R5T.Angleterria.Default/Code/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Dacia;
+
+
+namespace R5T.Angleterria.Default
+{
+    public static class IServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds the <see cref="DefaultVisualStudioStringlyTypedPathPartsOperator"/> implementation of <see cref="IVisualStudioStringlyTypedPathPartsOperator"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static IServiceCollection AddDefaultVisualStudioStringlyTypedPathPartsOperator(this IServiceCollection services)
+        {
+            services.AddSingleton<IVisualStudioStringlyTypedPathPartsOperator, DefaultVisualStudioStringlyTypedPathPartsOperator>();
+
+            return services;
+        }
+
+        /// <summary>
+        /// Adds the <see cref="DefaultVisualStudioStringlyTypedPathPartsOperator"/> implementation of <see cref="IVisualStudioStringlyTypedPathPartsOperator"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static ServiceAction<IVisualStudioStringlyTypedPathPartsOperator> AddDefaultVisualStudioStringlyTypedPathPartsOperatorAction(this IServiceCollection services)
+        {
+            var serviceAction = new ServiceAction<IVisualStudioStringlyTypedPathPartsOperator>(() => services.AddDefaultVisualStudioStringlyTypedPathPartsOperator());
+            return serviceAction;
+        }
+    }
+}
