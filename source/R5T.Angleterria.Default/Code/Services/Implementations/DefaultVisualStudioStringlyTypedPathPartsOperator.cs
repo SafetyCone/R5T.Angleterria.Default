91ee840f8e8b22e41a4d1e6115e779e7028a4ca1
davidcoats <david@getsafetycone.com>
2022-04-27 15:24:59 -0700

    Interim changes.

---
+++ source/R5T.Angleterria.Default/Code/Services/Implementations/DefaultVisualStudioStringlyTypedPathPartsOperator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using R5T.T0064;
+
+
+namespace R5T.Angleterria.Default
+{
+    [ServiceImplementationMarker]
+    public class DefaultVisualStudioStringlyTypedPathPartsOperator : IVisualStudioStringlyTypedPathPartsOperator, IServiceImplementation
+    {
+        /// <summary>
+        /// The default project directory name is the same as the project name.
+        /// </summary>
+        public string GetProjectDirectoryNameForProjectName(string projectName)
+        {
+            var projectDirectoryName = projectName;
+            return projectName;
+        }
+    }
+}
